83839c021cfa16c223ff2d36bb9d6bae42e69af6
joshennis30-netizen <joshennis30@gmail.com>
2025-12-02 09:38:24 -0330

    Add files via upload

--- PythonProject.py
+++ PythonProject.py
@@ -1,13 +1,28 @@
+import random
 
+def read_money():
+    money = 0
+    with open("money.txt") as file:
+        money = file.read()
+    print(money)
+    return money
+
+def write_money(money):
+    with open("money.txt") as file:
+        file.write(money)
 
 
 
 
 def main():
+    money = 0
     print("BLACKJACK!")
     print("Blackjack payout is 3:2")
     print()
-
+    money = read_money()
+    print(f"Money: {money}")
+    write_money(money)
+    
 
 if __name__ == "__main__":
     main()
