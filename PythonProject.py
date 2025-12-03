6696e8b40d99d7f123962cd91e7c2c1610fac896
joshennis30-netizen <joshennis30@gmail.com>
2025-12-03 16:33:19 -0330

    Add files via upload

--- PythonProject.py
+++ PythonProject.py
@@ -10,17 +10,36 @@ def bet(bet_amount, outcome, money):
         bet_amount = round(bet_amount, 2)
         money += bet_amount
     
-def cards_deck():
+def cards_deck(deck):
+    values = [11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     cards = ["Hearts", "Diamonds", "Clubs", "Spades"]
-    deck = []
+    faces = ["Jack", "King", "Queen"]
     for card in cards:
-        for i in range(1, 13):
+        deck.append([11, "of", card])
+    for card in cards:
+        for i in range(2, 10):
             deck.append([i, "of", card])
-    print(deck[0])
+    for card in cards:
+        for face in faces:
+            deck.append([face, "of", card])
+
+def dealer_hand(deck):
+    deal_hand = []
+    hand = random.randrange(1, len(deck))
+    deal_hand.append(deck[hand])
+    deck.pop[hand]
+
+def player_hand(deck):
+    play_hand = []
+    hand = random.randrange(1, len(deck))
+    play_hand.append(deck[hand])
+    deck.pop[hand]
 
 
 
 def main():
+    deck = []
+    turn = 0
     money = 0
     print("BLACKJACK!")
     print("Blackjack payout is 3:2")
@@ -28,8 +47,11 @@ def main():
     money = read_money(money)
     print(f"Money: {money}")
     bet_amount = float(input("Bet amount: "))
+    if bet_amount < 5 or bet_amount > 1000:
+        print("Bet amount must be more than 5 and less than 1000. Try again.")
     print()
-    cards_deck()
+    cards_deck(deck)
+    
     
 
 if __name__ == "__main__":
